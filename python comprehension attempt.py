6dc1ea98032ebaf068e75f2c3e1d49f417a4d934
AndrewSt86 <32872262+AndrewSt86@users.noreply.github.com>
2018-10-31 17:23:33 +0200

    Add files via upload

---
+++ python comprehension attempt.py
@@ -0,0 +1,5 @@
+dishes_string = input('What would you like? ' )
+	dishes_list = []
+		[print(set (dishes_list.append(dish)for dish in dishes_string.strip().split())]
+		[print(dishes_list.capitalize().ljust(50,'.'), random.randint(0, 60), 'minutes')for dises_list in set (dishes_list.append(dish)for dish in dishes_string.strip().split())]
+	
