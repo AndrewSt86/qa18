5f4c1b31492cf7c91af38cfade244463a70de08d
AndrewSt86 <32872262+AndrewSt86@users.noreply.github.com>
2018-11-05 14:10:02 +0200

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,9 @@
+n = 15
+    for num in range(1, n):
+        string = '' 
+        if(num % 3 = 0):
+            return string + "Fizz"
+        elif(num % 5 = 0):
+            return string + "Buzz"
+        elif(num % 5 = 0 && num % 3 = 0):
+            return string + "FizzBuzz"
